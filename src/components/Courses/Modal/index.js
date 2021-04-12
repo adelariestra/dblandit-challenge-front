ab79cd6e32a7b1042882061aecd4a7fb558f2021
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-12 15:00:00 -0300

    cleaned course modal

--- src/components/Courses/Modal/index.js
+++ src/components/Courses/Modal/index.js
@@ -1,12 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -16,7 +11,6 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
-import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
     form: {
