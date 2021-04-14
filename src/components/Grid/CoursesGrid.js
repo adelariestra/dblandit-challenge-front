7e1860476dbcfc15f58bdf649d6ff219e7bdf978
adelariestra <48964880+adelariestra@users.noreply.github.com>
2021-04-14 10:32:15 -0300

    added template of server querying

--- src/components/Grid/CoursesGrid.js
+++ src/components/Grid/CoursesGrid.js
@@ -3,18 +3,35 @@ import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
-import {DataGrid} from '@material-ui/data-grid';
+import { DataGrid } from '@material-ui/data-grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 
+const loadServerRows = (filterValue, rows) => {
+    return new Promise((resolve) => {
+
+        if (!filterValue) {
+            resolve(rows);
+            return;
+        }
+        resolve(
+            rows.filter(
+                (row) => row.theme.toLowerCase().indexOf(filterValue) > -1,
+            ),
+        );
+    });
+}
+
+
+
 const CourseGrid = (props) => {
     const [filterValue, setFilterValue] = React.useState();
     const [loading, setLoading] = React.useState(false);
-  
+
     const onFilterChange = React.useCallback((params) => {
-      setFilterValue(params.filterModel.items[0].value);
+        setFilterValue(params.filterModel.items[0].value);
     }, []);
 
     const useStyles = makeStyles((theme) => ({
@@ -37,12 +54,12 @@ const CourseGrid = (props) => {
 
     return (
         <Container className={classes.cardGrid} >
-            <DataGrid 
-            rows={props.cards.map((card)=>{return({...card,id:card._id})})}
-            columns={[{field:'_id',width:150},{field:'theme',width:150},{year:'year',width:150}]}
-            filterMode="server"
-            onFilterModelChange={onFilterChange}
-            loading={loading}/>
+            <DataGrid
+                rows={props.cards.map((card) => { return ({ ...card, id: card._id }) })}
+                columns={[{ field: '_id', width: 150 }, { field: 'theme', width: 150 }, { year: 'year', width: 150 }]}
+                filterMode="server"
+                onFilterModelChange={onFilterChange}
+                loading={loading} />
         </Container>
 
     );
